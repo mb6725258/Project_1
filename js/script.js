6da255b452fb185cb48198074b95893b268c7885
Matvey <mb6725258@gmail.com>
2022-03-10 19:26:21 +0300

    Corrected2

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 if (numberOfFilms < 10) {
     alert('Просмотрено довольно мало фильмов');
+    console.log('correct1');
 } else if (numberOfFilms <= 30) {
     alert('Вы классический зритель');
+    console.log('correct1');
 } else if (numberOfFilms > 30) {
     alert('Вы киноман');
+    console.log('correct1');
 } else {
     alert('Произошла ошибка!');
+    console.log('error1');
 }
 
 const personalMovieDB = {
@@ -21,19 +25,16 @@ const personalMovieDB = {
     private: false
 };
 
-let tmp1,
-    tmp2;
 let count = 0;
-while (count != 2) {
-    tmp1 = prompt("Один из последних просмотренных фильмов?", "");
-    tmp2 = prompt("На сколько вы оцените его?", "");
-    if (tmp1 == '' || tmp2 == '' || tmp1 == null || tmp2 == null || tmp1.length > 50) {
-        alert('Вы ввели некорректные значения!');
-        continue;
-    } else {
+for (let i = 0; i < 2; i++) {
+    const tmp1 = prompt("Один из последних просмотренных фильмов?", ""),
+          tmp2 = prompt("На сколько вы оцените его?", "");
+    if (tmp1 != '' && tmp2 != '' && tmp1 != null && tmp2 != null && tmp1.length < 50) {
         personalMovieDB.movies[tmp1] = tmp2;
-        count++;
+        console.log('correct2');
+    } else {
+        alert('Вы ввели некорректные значения! Попробуйте ещё раз');
+        i--;
+        console.log('error2');
     }
 }
-
-console.log(personalMovieDB);
