831b4d304548d8dad43f33da147c74ec4db2585e
Matvey <mb6725258@gmail.com>
2022-03-10 19:09:35 +0300

    Second practice

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@
 
 //Унарный плюс делает значение числовым типом
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+if (numberOfFilms < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms <= 30) {
+    alert('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка!');
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,13 +21,19 @@ const personalMovieDB = {
     private: false
 };
 
-const lastFilm1 = prompt("Один из последних просмотренных фильмов?", ""),
-      lastFilm1Rate = prompt("На сколько вы оцените его?", ""),
-      lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
-      lastFilm2Rate = prompt("На сколько вы оцените его?", "");
+let tmp1,
+    tmp2;
+let count = 0;
+while (count != 2) {
+    tmp1 = prompt("Один из последних просмотренных фильмов?", "");
+    tmp2 = prompt("На сколько вы оцените его?", "");
+    if (tmp1 == '' || tmp2 == '' || tmp1 == null || tmp2 == null || tmp1.length > 50) {
+        alert('Вы ввели некорректные значения!');
+        continue;
+    } else {
+        personalMovieDB.movies[tmp1] = tmp2;
+        count++;
+    }
+}
 
-personalMovieDB.movies[lastFilm1] = lastFilm1Rate;
-personalMovieDB.movies[lastFilm2] = lastFilm2Rate;
-
-//Обязательны косые кавычки
-alert(`Вы недавно посмотрели фильмы ${lastFilm1}(${lastFilm1Rate}) и ${lastFilm2}(${lastFilm2Rate})`);
+console.log(personalMovieDB);
